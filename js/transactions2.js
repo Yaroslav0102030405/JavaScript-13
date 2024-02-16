e880e24423b3ffd4c98aabad74f487b7c6c10d21
Yaroslav Lukovetskyi <lukovetskyi@gmail.com>
2024-02-16 22:33:17 +0200

    0

--- js/transactions2.js
+++ js/transactions2.js
@@ -3,9 +3,9 @@ import transactionHistory from './transactions.js';
 
 // создали функцию которая возвращает одну строку разметки
 const makeTransactionTableRowmarkup = transaction => {
-    // сделали деструктуризацию
-  const { id, amount, date, bisiness, name, type, account } = transaction
-//   console.log(transaction);
+  // сделали деструктуризацию
+  const { id, amount, date, bisiness, name, type, account } = transaction;
+  //   console.log(transaction);
   return `
     <tr>
     <th>${id}</th>
@@ -23,11 +23,17 @@ console.log(transactionHistory);
 // console.log(makeTransactionTableRowmarkup(transactionHistory[2]));
 
 // получили доступ к ссылке класса
-const tableEl = document.querySelector('.js-transaction-table')
+const tableEl = document.querySelector('.js-transaction-table');
 
 // перебрали массив и разделили его на строки
-const makeTransactionTableRows = transactionHistory.map(makeTransactionTableRowmarkup).join('')
+const makeTransactionTableRows = transactionHistory
+  .map(makeTransactionTableRowmarkup)
+  .join('');
 
 // прицепили всю разметку внизу таблицы
-tableEl.insertAdjacentHTML('beforeend', makeTransactionTableRows)
-console.log(makeTransactionTableRows)
+tableEl.insertAdjacentHTML('beforeend', makeTransactionTableRows);
+// console.log(makeTransactionTableRows)
+
+const www = document.querySelector('.js-transaction-table-2');
+
+www.insertAdjacentHTML('beforeend', makeTransactionTableRows);
