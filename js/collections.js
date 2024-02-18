b1cde300664f8891eb2c1bb519ace6aac9bdb4d7
Yaroslav Lukovetskyi <lukovetskyi@gmail.com>
2024-02-18 20:30:30 +0200

    0

--- js/collections.js
+++ js/collections.js
@@ -1,16 +1,31 @@
 // Создаем и добавляем коллекцию
 
-const colorPickerOptions = [
-  { label: 'red', color: '#f44336' },
-  { label: 'green', color: '#4CAF50' },
-  { label: 'blue', color: '#2196F3' },
-  { label: 'grey', color: '#607D8B' },
-  { label: 'pink', color: '#E91E63' },
-  { label: 'indigo', color: '#3F51B5' },
-];
+import colorPickerOptions from '../data/color.json' assert { type: 'json' };
+// console.log(colorPickerOptions);
+
+// console.log(colorPickerOptions);
+
+// const colorPickerOptions = [
+//   { label: 'red', color: '#f44336' },
+//   { label: 'green', color: '#4CAF50' },
+//   { label: 'blue', color: '#2196F3' },
+//   { label: 'grey', color: '#607D8B' },
+//   { label: 'pink', color: '#E91E63' },
+//   { label: 'indigo', color: '#3F51B5' },
+// ];
 
 // Пишем функцию для создания колорпикера
 // мы создадим для каждого обьекта кнопку
+
+// async function getProducts() {
+//   try {
+//     const dog = await fetch('../data/color.json');
+//     const colorPickerOptions = JSON.parse(dog);
+//     console.log(colorPickerOptions);
+//   } catch (error) {
+//     console.log(error);
+//   }
+// }
 
 const colorPickerContainerEl = document.querySelector('.js-color-picker');
 
@@ -49,7 +64,7 @@ const makeColorPickerOptions = options => {
     buttonEl.tyoe = 'button';
     // создали название кнопки
     buttonEl.textContent = option.label;
-    buttonEl.classList.add('button-colorPicker')
+    buttonEl.classList.add('button-colorPicker');
     buttonEl.style.backgroundColor = option.color;
 
     return buttonEl;
@@ -58,3 +73,12 @@ const makeColorPickerOptions = options => {
 
 const elements = makeColorPickerOptions(colorPickerOptions);
 colorPickerContainerEl.append(...elements);
+
+// async function getproducts() {
+//   try {
+//     const res = (await fetch('../data/products.json')).json();
+//     console.log(res);
+//   } catch (error) {
+//     console.log(error);
+//   }
+// }
